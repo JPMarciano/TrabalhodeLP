c98d2d9ba6883992d7d5e422417b8edec45fbb72
João Pedro de Abreu Marciano <jpdeabremarciano@gmail.com>
2019-09-04 19:50:02 -0300

    criando atributos

--- Pessoa.py
+++ Pessoa.py
@@ -1,3 +1,8 @@
 class Pessoa:
-    def __init__(self):  # adicionar 15 atributos a partir dos dados da PNAD de 2014
-        pass
+    def __init__(self, atributos):  # adicionar 15 atributos a partir dos dados da PNAD de 2014
+        self.rend_dom = atributos[0]  # redimento domiciliar
+        self.rede = atributos[1]  # rede de ensino
+        self.mod = atributos[2]  # modalidade
+        self.sexo = atributos[3]  # sexo
+        self.cor = atributos[4]  # cor ou raça
+
--- Populacao.py
+++ Populacao.py
@@ -1,4 +1,5 @@
 from Pessoa import Pessoa
+from random import uniform
 import numpy as np
 
 '''
@@ -11,6 +12,9 @@ class Populacao:
     def __init__(self, tamanho=1000):
         self.tamanho = tamanho
         self.individuos = []
+
+        porcentagem = 1/100[[2.3, 6.5, 10.8, 24.4, 34.8, 12.6, 3.1, 5.5], [], [], [], []]  # lista das porcentagens de pessoas em cada categoria
+        dv = [[], [], [], [], []]  # desvio padrao de cada categoria
 
     def amostra(self, n):
         amostrapop = np.random.choice(self.individuos, n)
