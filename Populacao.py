048a8d7d5521ca631d028a620850361619893f6d
rod-ribs <52456418+rod-ribs@users.noreply.github.com>
2019-09-05 16:47:39 -0300

    Update Populacao.py

--- Populacao.py
+++ Populacao.py
@@ -24,7 +24,6 @@ def fun(a, w):
             if b[n-1] < z <= b[n]:
                 c[n] += 1
     return c
-print(fun([2,3,4,1,50,20,10,10],1000))
 
 
 
@@ -32,10 +31,47 @@ class Populacao:
     def __init__(self, tamanho=1000):
         self.tamanho = tamanho
         self.individuos = []
+        cat=np.array([[], [], [], [], []])
 
-        porcentagem = (1/100)*[[2.3, 6.5, 10.8, 24.4, 34.8, 12.6, 3.1, 5.5], [], [], [], []]  # lista das porcentagens de pessoas em cada categoria
-        dv = [[], [], [], []]  # desvio padrao de cada categoria
-        faixas = [[], [], [], [], []]
+        porcentagem = np.array([[2.3, 6.5, 10.8, 24.4, 34.8, 12.6, 3.1, 5.5], [], [], [], []])*(1/100)  # lista das porcentagens de pessoas em cada categoria
+        dv = np.array([[], [], [], [],[]])*(1/100) # desvio padrao de cada categoria
+        faixas = np.array([[], [], [], [], []])
+        for u in range(5):
+            v=dv[u]
+            v=np.array(v)
+            r=porcentagem[u]
+            r=np.array(r)
+            dv[u]=r*v
+        for q in range(5):
+            v=dv[q]
+            v=np.array(v)
+            e=random.uniform(-1,1)
+            v=v*e
+            r=porcentagem[q]
+            r=np.array[r]
+            faixas[q]=r+v
+        c=0
+        for j in faixas:
+            cat[c] = fun(j,tamanho)
+            c += 1
+        
+        popfin = np.zeros((tamanho,5))
+        inicio = np.array(list(range(tamanho)))
+        np.random.shuffle(inicio)
+        d=0
+        for i in cat:
+            ind3 = cat.index(i)
+            p = 0
+            for j in range(len(i)):
+                if j==0:
+                else:
+                    p += i[j-1]
+                    for k in range(p,i[j]+p):
+                        popfin[inicio[k],ind3] = j
+        
+                
+        self.individuos = [Pessoas(g) for g in popfin]
+            
         atributos = [['de 0 a 1 ', 'mulher']]
 
     def amostra(self, n):
